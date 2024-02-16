eda4ddef0f411c211dbe604f57e9d86fe59310c9
Ivan Gagis <ivan.gagis@ge.com>
2024-02-16 14:52:39 +0200

    license

--- src/serial_port.hpp
+++ src/serial_port.hpp
@@ -1,7 +1,7 @@
 /*
 bedsidemon - Bed-side monitor example GUI project
 
-Copyright (C) Ivan Gagis  <igagis@gmail.com>
+Copyright (C) 2024 Ivan Gagis  <igagis@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
