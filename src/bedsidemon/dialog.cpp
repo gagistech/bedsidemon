61f1805100d3c9d1c8ab09e26629d6221f6e3bf9
Ivan Gagis <igagis@gmail.com>
2024-09-13 14:30:55 +0300

    dialog

---
+++ src/bedsidemon/dialog.cpp
@@ -0,0 +1,133 @@
+/*
+bedsidemon - Bedside monitor example GUI project
+
+Copyright (C) 2024 Gagistech Oy <gagistechoy@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/* ================ LICENSE END ================ */
+
+#include "dialog.hpp"
+
+#include <ruis/widget/label/nine_patch.hpp>
+#include <ruis/widget/label/rectangle.hpp>
+#include <ruis/widget/proxy/mouse_proxy.hpp>
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+using namespace ruis;
+
+namespace m {
+using namespace ruis::make;
+} // namespace m
+
+namespace {
+constexpr auto color_dialog_surroundings = 0xa0000000;
+} // namespace
+
+namespace {
+std::vector<utki::shared_ref<widget>> make_root_widget_structure(
+	utki::shared_ref<ruis::context> c, //
+	container::parameters container_params,
+	utki::span<const utki::shared_ref<widget>> contents
+)
+{
+	// clang-format off
+    return {
+        m::mouse_proxy(c,
+            {
+                .layout_params{
+                    .dims{ruis::dim::fill, ruis::dim::fill}
+                },
+                .widget_params{
+                    .id = "ruis_mouse_proxy"s
+                }
+            }
+        ),
+        m::rectangle(c,
+            {
+                .layout_params{
+                    .dims{ruis::dim::fill, ruis::dim::fill}
+                },
+                .color_params{
+                    .color = color_dialog_surroundings
+                }
+            }
+        ),
+        m::nine_patch(c,
+            {
+                .widget_params{
+                    .id = "ruis_nine_patch"s
+                },
+                .container_params = std::move(container_params),
+                .nine_patch_params{
+                    .nine_patch = c.get().loader.load<ruis::res::nine_patch>("ruis_npt_window_bg")
+                }
+            },
+            contents
+        )
+    };
+	// clang-format on
+}
+} // namespace
+
+dialog::dialog(
+	utki::shared_ref<ruis::context> context, //
+	all_parameters params,
+	utki::span<const utki::shared_ref<widget>> contents
+) :
+	widget(
+		std::move(context), //
+		{
+			.dims{ruis::dim::max, ruis::dim::max}  //
+},
+		std::move(params.widget_params)
+	),
+	container(
+		this->context, //
+		{
+			.container_params{
+				.layout = ruis::layout::pile //
+			} //
+		},
+		make_root_widget_structure(
+			this->context, //
+			std::move(params.container_params),
+			contents
+		)
+	)
+{
+	{
+		auto& mp = this->get_widget_as<ruis::mouse_proxy>("ruis_mouse_proxy"sv);
+		mp.mouse_button_handler = [this](ruis::mouse_proxy&, const ruis::mouse_button_event& e) {
+			this->on_dismiss();
+			// consume event
+			return true;
+		};
+	}
+}
+
+void dialog::on_dismiss()
+{
+	this->close();
+}
+
+void dialog::close()
+{
+	this->context.get().post_to_ui_thread([d = utki::make_shared_from(*this)]() {
+		d.get().remove_from_parent();
+	});
+}
---
+++ src/bedsidemon/dialog.hpp
@@ -0,0 +1,49 @@
+/*
+bedsidemon - Bedside monitor example GUI project
+
+Copyright (C) 2024 Gagistech Oy <gagistechoy@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/* ================ LICENSE END ================ */
+
+#pragma once
+
+#include <ruis/widget/container.hpp>
+
+namespace ruis {
+
+class dialog :
+	virtual public widget, //
+	private container
+{
+public:
+	struct all_parameters {
+		widget::parameters widget_params;
+		container::parameters container_params;
+	};
+
+	dialog(
+		utki::shared_ref<ruis::context> context, //
+		all_parameters params,
+		utki::span<const utki::shared_ref<widget>> contents
+	);
+
+	void close();
+
+	virtual void on_dismiss();
+};
+
+} // namespace ruis
