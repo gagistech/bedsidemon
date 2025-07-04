c39578e0cb51edf173250af0710bbaf637496799
Ivan Gagis <igagis@gmail.com>
2025-07-04 15:10:31 +0300

    fix build

--- src/bedsidemon/settings.cpp
+++ src/bedsidemon/settings.cpp
@@ -23,9 +23,9 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <filesystem>
 
-#include <utki/config.hpp>
 #include <papki/fs_file.hpp>
 #include <ruisapp/application.hpp>
+#include <utki/config.hpp>
 
 using namespace std::string_view_literals;
 
