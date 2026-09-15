a210906f6a3942fdc1d731b867e80590a5b049be
Ivan Gagis <igagis@gmail.com>
2026-09-15 17:28:16 +0300

    update

--- src/version.cpp
+++ src/version.cpp
@@ -1,3 +1,3 @@
-// this file is a dummy version fil to use in build systems which don't have version file genreation implemented
+// this file is a dummy version file to use in build systems which don't have version file genreation implemented
 
 extern const char* const program_version = "0.0.0";
