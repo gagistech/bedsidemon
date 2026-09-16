7e1d1917388c6632d5188619498421e2fb3b54aa
Ivan Gagis <igagis@gmail.com>
2026-09-16 21:44:11 +0300

    update

--- src/bedsidemon/gui.cpp
+++ src/bedsidemon/gui.cpp
@@ -117,8 +117,8 @@ utki::shared_ref<ruis::rectangle> make_separator(utki::shared_ref<ruis::context>
 		{
 			.layout_params{
 				.dims = {
-					vertical ? ruis::layout::dimension(1_pp) : ruis::layout::dimension(ruis::dim::fill),
-					vertical ? ruis::layout::dimension(ruis::dim::fill) : ruis::layout::dimension(1_pp)
+					vertical ? ruis::dimension(1_pp) : ruis::dimension(ruis::dim::fill),
+					vertical ? ruis::dimension(ruis::dim::fill) : ruis::dimension(1_pp)
 				}
 			},
 			.params{
